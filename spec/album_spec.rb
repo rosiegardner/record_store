ad22e9b7322efe605efd066bc5c5af3e29479574
Rosie Gardner <rosiegardner78@gmail.com>
2022-05-14 14:14:56 -0700

    Update testing for album.rb - Passes all tests

--- spec/album_spec.rb
+++ spec/album_spec.rb
@@ -2,81 +2,82 @@ require('spec_helper')
 
 describe '#Album' do
 
-  # describe('#save') do
-  #   it("saves an album") do
-  #     album = Album.new("Giant Steps", nil) # nil added as second argument
-  #     album.save()
-  #     album2 = Album.new("Blue", nil) # nill added as second argument
-  #     album2.save()
-  #     expect(Album.all).to(eq([album, album2]))
-  #   end
-  # end
+  describe('#save') do
+    it("saves an album") do
+      album = Album.new({:name => "Giant Steps",:id => nil}) # nil added as second argument
+      album.save()
+      album2 = Album.new({:name => "Blue",:id => nil}) # nill added as second argument
+      album2.save()
+      expect(Album.all).to(eq([album, album2]))
+    end
+  end
   
-  # describe('.all') do
-  #   it("returns an empty array when there are no albums") do
-  #     expect(Album.all).to(eq([]))
-  #   end
-  # end
+  describe('.all') do
+    it("returns an empty array when there are no albums") do
+      expect(Album.all).to(eq([]))
+    end
+  end
 
-  # describe('.all') do
-  #   it("returns an array of all albums in the database") do
-  #     album = Album.new("Giant Steps", nil)
-  #     album.save
-  #     album2 = Album.new("Blue", nil)
-  #     album2.save
-  #     expect(Album.all).to(eq([album, album2]))
-  #   end
-  # end
+  describe('.all') do
+    it("returns an array of all albums in the database") do
+      album = Album.new({:name => "Giant Steps",:id => nil})
+      album.save
+      album2 = Album.new({:name => "Blue",:id => nil})
+      album2.save
+      expect(Album.all).to(eq([album, album2]))
+    end
+  end
   
    
-  # describe('#==') do
-  #   it("is the same album if it has the same attributes as another album") do
-  #     album = Album.new("Blue", nil)
-  #     album2 = Album.new("Blue", nil)
-  #     expect(album).to(eq(album2))
-  #   end
-  # end
+  describe('#==') do
+    it("is the same album if it has the same attributes as another album") do
+      album = Album.new({:name => 'Blue',:id => nil})
+      album2 = Album.new({:name => 'Blue',:id => nil})
+      # expect(album).to(eq(album2))
+      expect(album == album2).to(eq(true))
+    end
+  end
   
-  # describe('.clear') do
-  #   it("clears all albums") do
-  #     album = Album.new("Giant Steps", nil)
-  #     album.save()
-  #     album2 = Album.new("Blue", nil)
-  #     album2.save()
-  #     Album.clear()
-  #     expect(Album.all).to(eq([]))
-  #   end
-  # end
+  describe('.clear') do
+    it('clears all albums') do
+      album = Album.new(:name =>'Giant Steps',:id => nil)
+      album.save()
+      album2 = Album.new(:name =>'Blue',:id => nil)
+      album2.save()
+      Album.clear
+      expect(Album.all).to(eq([]))
+    end
+  end
 
-  # describe('.find') do
-  #   it("finds an album by id") do
-  #     album = Album.new("Giant Steps", nil)
-  #     album.save()
-  #     album2 = Album.new("Blue", nil)
-  #     album2.save()
-  #     expect(Album.find(album.id)).to(eq(album))
-  #   end
-  # end
+  describe('.find') do
+    it('finds an album by id') do
+      album = Album.new(:name => 'Giant Steps',:id => nil)
+      album.save()
+      album2 = Album.new(:name =>'Blue',:id => nil)
+      album2.save()
+      expect(Album.find(album.id)).to(eq(album))
+    end
+  end
 
-  # describe('#update') do
-  #   it("updates an album by id") do
-  #     album = Album.new("Giant Steps", nil)
-  #     album.save()
-  #     album.update("A Love Supreme")
-  #     expect(album.name).to(eq("A Love Supreme"))
-  #   end
-  # end 
+  describe('#update') do
+    it('updates an album by id') do
+      album = Album.new(:name =>'Giant Steps',:id => nil)
+      album.save()
+      album.update('A Love Supreme')
+      expect(album.name).to(eq('A Love Supreme'))
+    end
+  end 
 
-  # describe('#delete') do
-  #   it("deletes an album by id") do
-  #     album = Album.new("Giant Steps", nil)
-  #     album.save()
-  #     album2 = Album.new("Blue", nil)
-  #     album2.save()
-  #     album.delete()
-  #     expect(Album.all).to(eq([album2]))
-  #   end
-  # end
+  describe('#delete') do
+    it("deletes an album by id") do
+      album = Album.new(:name =>"Giant Steps",:id => nil)
+      album.save()
+      album2 = Album.new(:name =>"Blue",:id => nil)
+      album2.save()
+      album.delete()
+      expect(Album.all).to(eq([album2]))
+    end
+  end
 
   # describe('.search') do
   #   it("searches for an album by album name") do
