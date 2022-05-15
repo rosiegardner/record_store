e762b2f6144f104763571300eac7910f4123b312
Rosie Gardner <rosiegardner78@gmail.com>
2022-05-15 16:36:21 -0700

    update database - comments

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -5,12 +5,14 @@ require 'song'
 require 'artist'
 require 'pry'
 
+# shared code for clearing tests between runs & connecting to the DB:
+
 DB = PG.connect({:dbname => 'record_store_test'})
 
 RSpec.configure do |config|
   config.after(:each) do
     DB.exec("DELETE FROM albums *;")
     DB.exec("DELETE FROM songs *;")
-    DB.exec("DELETE FROM artist *;")
+    DB.exec("DELETE FROM artists *;")
   end
 end
