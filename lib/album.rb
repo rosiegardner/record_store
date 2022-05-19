b3a824b1063619ef3ab18003bbdf839a5974a8e5
Rosie Gardner <rosiegardner78@gmail.com>
2022-05-18 20:37:52 -0700

    update functionality to update method and add search method - passes tests

--- lib/album.rb
+++ lib/album.rb
@@ -42,10 +42,23 @@ class Album
     self.name() == album_to_compare.name()
   end
   
-  def update(x)
-    @name = x
-    DB.exec("UPDATE albums SET name = '#{@name}' WHERE id = #{@id};")
-  end
+  def update(attributes) 
+    if (attributes.has_key?(:name)) && (attributes.fetch(:name) != nil)
+      @name = attributes.fetch(:name) 
+      DB.exec("UPDATE albums SET name = '#{@name}' WHERE id = #{@id};")
+    elsif (attributes.has_key?(:artist_name)) && (attributes.fetch(:artist_name) != nil)
+      artist_name = attributes.fetch(:artist_name)
+      artist = DB.exec("SELECT * FROM artists WHERE lower(name)='#{artist_name.downcase}';").first
+      if artist != nil
+        DB.exec("INSERT INTO albums_artists (artist_id, album_id) VALUES (#{artist['id'].to_i}, #{@id});")
+      end
+    end
+  end 
+
+  # def update(x)
+  #   @name = x
+  #   DB.exec("UPDATE albums SET name = '#{@name}' WHERE id = #{@id};")
+  # end
   
   def delete
     DB.exec("DELETE FROM albums WHERE id = #{@id};")
@@ -53,13 +66,42 @@ class Album
   end
   
   def songs
-    Song.find_by_album(self.id)
+    find = Song.find_by_album(self.id)
+    if find.any?
+      find
+    end
+  end
+
+  # def songs
+  #   Song.find_by_album(self.id)
+  # end
+
+  def artists
+    artists = []
+    results = DB.exec("SELECT artist_id FROM albums_artists WHERE album_id = #{@id};")
+    results.each() do |result|
+      artist_id = result.fetch("artist_id").to_i()
+      artist = DB.exec("SELECT * FROM artists WHERE id = #{artist_id};")
+      name = artist.first().fetch("name")
+      artists.push(Artist.new({:name => name, :id => artist_id}))
+    end
+    if artists.any?
+      artists
+    end
   end
   
-  # def self.search(str)
-  #   album_search = @@albums.find { |album| album[1].name.downcase == str.downcase }
-  #   album_search[1] 
-  # end
+  def self.search(str)
+    result = DB.exec("SELECT * FROM albums WHERE name ILIKE '%#{str}%';")
+    albums = []
+    result.each() do |album|
+      name = album.fetch("name")
+      id = album.fetch("id").to_i
+      albums.push(Album.new({:name => name, :id => id}))
+    end
+    if albums.any?
+      albums
+    end
+  end
   
   
 end
